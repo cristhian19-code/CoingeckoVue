3cbe1a6ca81e73372b94db9a9d0f4fbb43c98034
cristhian19-code <cristhianprozqwerty@gmail.com>
2021-10-12 12:49:34 -0500

    articles app vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="darkmode ? 'uk-background-secondary': ''" class="home uk-padding">
+    <div class="uk-flex uk-flex-between uk-flex-middle">
+      <h1 :class="darkmode ? 'text-white':''" class="uk-text-lighter">Coin Market</h1>
+      <div>
+        <button @click="darkmode = !darkmode" uk-icon="cog"></button>
+      </div>
+    </div>
+    <form class="uk-width-expand uk-search uk-search-default uk-margin-small-top uk-margin-small-bottom">
+      <input class="uk-search-input" type="search" placeholder="Search" @keyup="handleSearch">
+    </form>
+    <table class="uk-table uk-table-divider uk-table-responsive uk-table-hover">
+      <thead>
+          <tr>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Price Change(%)</th>
+              <th>Volumen 24h</th>
+          </tr>
+      </thead>
+      <tbody v-if="filterSearch.length>0">
+          <CoinItem v-for="coin in filterSearch" :key="coin.id" :coin="coin"/>
+      </tbody>
+      <p v-else>no hay resultados</p>
+    </table>
+    <div class="list-pages uk-flex uk-flex-center">
+      <button @click="handleChangePages" v-for="page in listpage" :key="page" class="uk-button-primary" style="padding: 5px 10px;border-radius: 3px">{{page}}</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CoinItem from "./components/CoinItem.vue"
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { 
+    CoinItem
+  },
+  data() {
+    return {
+      coin_data: [],
+      filterSearch: [],
+      listpage: [1,2,3,4,5,6,7],
+      darkmode: false
+    }
+  },
+  methods: {
+    handleSearch(e){
+      e.preventDefault();
+      
+      this.filterSearch = this.coin_data.filter(coin => 
+        coin.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+    },
+    handleChangePages(e){
+      this.fetchCoins(parseInt(e.target.textContent))
+    },
+    async fetchCoins(value){
+      const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=8&page=${value}&sparkline=false`) 
+      const data = await res.json();
+      this.coin_data = data;
+      this.filterSearch = data;
+    },
+  },
+  async mounted() {
+    this.fetchCoins(1)
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.home{
+  min-height: 100vh;
+}
+
+.text-white{
+  color: white;
+}
+
+.list-pages{
+  grid-gap: 6px;
+}
+
+.list-pages button{
+  cursor: pointer;
 }
 </style>
---
+++ src/components/CoinItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <tr>
+        <td>{{coin.name}}</td>
+        <td>{{coin.symbol}}</td>
+        <td>
+            <img :src="coin.image" alt="">
+        </td>
+        <td>${{coin.current_price}}</td>
+        <td :class="coin.price_change_percentage_24h > 0?'uk-text-success' : 'uk-text-danger'">{{coin.price_change_percentage_24h}}%</td>
+        <td>${{coin.total_volume.toLocaleString()}}</td>
+
+    </tr>
+</template>
+
+<script>
+export default {
+    props: {
+        coin: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style>
+img{
+    height: 30px;
+    width: 30px;
+}
+</style>
